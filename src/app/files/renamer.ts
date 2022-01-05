d267d9693166fefb7388be7655a3c7ef0c98abd1
claus <claus.z@protonmail.com>
2022-01-05 22:36:03 +1100

    Hacky date parsing attempt
    This is strictly temporary.

--- src/app/files/renamer.ts
+++ src/app/files/renamer.ts
@@ -20,17 +20,15 @@ export function rename(file: any, newName: string) {
   );
 }
 
-export function findNewName(file: File): Promise<string> {
-  return exifr
-    .parse(file)
-    .then((output: any) => {
-      return `${formatDate(output.DateTimeOriginal)}.jpg`;
-    })
-    .catch((error) => {
-      return new Promise((resolve, reject) => {
-        resolve(parseDateFromString(file.name));
-      });
+export async function findNewName(file: File): Promise<string> {
+  try {
+    const output = await exifr.parse(file);
+    return `${formatDate(output.DateTimeOriginal)}.jpg`;
+  } catch (error) {
+    return await new Promise((resolve, reject) => {
+      resolve(parseDateFromString(file.name));
     });
+  }
 }
 
 function formatDate(date: Date): string {
@@ -51,7 +49,7 @@ function zeroPad(value: number): string {
 function parseDateFromString(name: string): string {
   for (const pattern of patterns) {
     if (pattern && pattern.pattern.test(name)) {
-      return pattern.func(name);
+      return dateBitsToString(pattern.func(name));
     }
   }
   return 'bad file';
@@ -60,27 +58,73 @@ function parseDateFromString(name: string): string {
 const patterns = [
   {
     pattern: /\d\d\d\d:\d\d:\d\d \d\d:\d\d:\d\d\.jpg$/,
-    func: (name: string): string => {
-      return '';
+    func: (name: string): DateBits => {
+      return {
+        year: name.substring(0, 4),
+        month: name.substring(6, 7),
+        day: name.substring(9, 10),
+        hour: name.substring(12, 13),
+        minute: name.substring(15, 16),
+        second: name.substring(18, 19),
+        extension: name.substring(21, 23),
+      };
     },
   },
   {
     pattern: /(IMG|VID)_\d{8}_\d{6}.(jpg|mp4)$/,
-    func: (name: string): string => {
-      return '';
+    func: (name: string): DateBits => {
+      return {
+        year: name.substring(4, 7),
+        month: name.substring(8, 9),
+        day: name.substring(10, 11),
+        hour: name.substring(13, 14),
+        minute: name.substring(15, 16),
+        second: name.substring(17, 18),
+        extension: name.substring(20, 22),
+      };
     },
   },
   {
     pattern: /MTGA \d_\d\d_\d\d\d\d \d_\d\d_\d\d (am|pm|AM|PM).(png|mp4)$/,
-    func: (name: string): string => {
-      return '';
+    func: (name: string): DateBits => {
+      return {
+        year: name.substring(0, 4),
+        month: name.substring(6, 7),
+        day: name.substring(9, 10),
+        hour: name.substring(12, 13),
+        minute: name.substring(15, 16),
+        second: name.substring(18, 19),
+        extension: name.substring(21, 23),
+      };
     },
   },
   ,
   {
     pattern: /^\d\d\d\d-\d\d-\d\d_(am|pm|AM|PM)-\d\d-\d\d-\d\d/,
-    func: (name: string): string => {
-      return '';
+    func: (name: string): DateBits => {
+      return {
+        year: name.substring(0, 4),
+        month: name.substring(6, 7),
+        day: name.substring(9, 10),
+        hour: name.substring(12, 13),
+        minute: name.substring(15, 16),
+        second: name.substring(18, 19),
+        extension: name.substring(21, 23),
+      };
     },
   },
 ];
+
+interface DateBits {
+  year: string;
+  month: string;
+  day: string;
+  hour: string;
+  minute: string;
+  second: string;
+  extension: string;
+}
+
+function dateBitsToString(bits: DateBits): string {
+  return `${bits.year}-${bits.month}-${bits.day}_${bits.hour}.${bits.minute}.${bits.second}`;
+}
