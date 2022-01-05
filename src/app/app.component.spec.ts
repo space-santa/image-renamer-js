d9f4f85085c9b6c6c98629181296fd7256a5f9ec
Claus <claus.z@protonmail.com>
2022-01-06 10:09:57 +1100

    Update app.component.spec.ts to new reality
    Haven't tested this in a while...

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FilesComponent } from './files/files.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [AppComponent, FilesComponent],
     }).compileComponents();
   });
 
@@ -18,14 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('image-renamer-js');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'image-renamer-js app is running!'
-    );
   });
 });
